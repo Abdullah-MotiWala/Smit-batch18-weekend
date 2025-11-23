36e05971949014ee1fda67eef12fe25eec051880
Abdullah Motiwala <motiwalaabdullah100@gmail.com>
2025-11-23 17:43:20 +0500

    23-11-25: js start, topic-1-5 covered

---
+++ js/app.js
@@ -0,0 +1,80 @@
+// alert("This is js");
+
+// var my name = 25; //space NOT allowed
+// var _myname = 25 // _ and $ allowed just
+// var 1myname = 25 // number at start NOT allowed
+// var myName1 = 25 number at the end and in between allowed
+
+var myFirstName = 25; //camel case
+var MyFirstName = 25; //Pascal case
+var my_first_name = 25; //snake case
+var MY_FIRST_NAME = 25; //screaming snake case
+
+// Data Types
+
+// Primitive
+var firstName = "Abdullah"; //string
+var firstName = "Abdullah"; //string
+var isMarried = true; // boolean e.g true/false
+var childrenCount = 4; //number
+var hasPowred = null; //null
+var reason = undefined; //undefined
+
+// Non Primitive
+// [], {}
+
+// var a;
+// a = 1;
+// var a = 3
+// var a = 2 //re initialize
+// a = 3 // new assignement
+
+var a = 4;
+a = 5;
+
+var greetings = "Hello, How're you?";
+// alert(greetings)
+
+var a = 1 + 1;
+var b = 2 - 1;
+var c = 2 * 2;
+var d = 6 / 3;
+// console.log("test")
+var e = 3 ** 4;
+// BODMAS | Bracket Open, Divide, Multiply, Addition, Substraction
+var g = (2 * 9) / (5 * 8 + 8) - 8 + 9;
+
+var BO1 = 18;
+var BO2 = 48;
+var division = BO1 / BO2;
+var addition = 17;
+var substraction = division - addition;
+var result = substraction;
+console.log(result);
+
+console.log(a, b, c, d, e);
+
+var f = "a" + "a"; //concatenat in case of addition
+var g = "1" * "1"; // "1" number like string
+var h = "1" + 1;
+var i = "10" / 5;
+// console.log(f)
+console.log(i);
+
+// var result2 = isNaN(f)
+// console.log(result2,"===result")
+
+var j = 9 % 2;
+console.log(j);
+
+// var openVar = 1
+// var openVar = 5 //allowed
+// openVar = 6 //allowed
+
+let openLet = 1
+openLet = 2 //allowed
+// let openLet = 1 //illegal
+
+const closedConst = 3.14
+// const closedConst = 3.15 // illegal
+// closedConst = 3.15 // illegal
