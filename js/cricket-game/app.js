6d34028ad5ad697506c7471d7a4e4ceeded92ec4
Abdullah Motiwala <motiwalaabdullah100@gmail.com>
2026-01-18 17:56:03 +0500

    18-01-26: side selection and play basic

--- js/cricket-game/app.js
+++ js/cricket-game/app.js
@@ -1,16 +1,32 @@
 const tossSides = ["hd", "tl"];
+const choices = ["bat", "bowl"];
+const playItems = ["score", "wicket", "extras"];
+
+const playItemDetails = {
+  score: [0, 1, 2, 3, 4, 6],
+  wicket: ["bowled", "caught", "run out", "lbw", "stumped"],
+  extras: ["wide", "no ball", "bye", "leg bye"],
+};
+
+playItemDetails[playItems[0]];
 
 let playerName = null;
 let teamId = null;
+let isTossWon = null;
+let userPlaying = null;
 
 const nameWrapper = document.getElementById("name-wrapper");
 const teamWrapper = document.getElementById("teams-wrapper");
 const tossWrapper = document.getElementById("toss-wrapper");
+const electedWrapper = document.getElementById("elected-wrapper");
+const playWrapper = document.getElementById("play-wrapper");
 // Get player name
 
 const nameNextButton = nameWrapper?.querySelector("#next");
 const teamNextButton = teamWrapper?.querySelector("#next");
 const tossNextButton = tossWrapper?.querySelector("#next");
+const electedNextButton = electedWrapper?.querySelector("#next");
+const playClickButton = playWrapper?.querySelector("#play");
 
 function getPlayerName() {
   const nameInput = nameWrapper.querySelector("#name");
@@ -48,6 +64,7 @@ function teamNextClickHanlder() {
 }
 teamNextButton?.addEventListener("click", teamNextClickHanlder);
 
+// Toss
 function tossClickHandler(event) {
   const toss = event.target;
   const tossId = toss.id;
@@ -55,8 +72,10 @@ function tossClickHandler(event) {
   const winToss = tossSides[tossNumber];
 
   if (tossId === winToss) {
+    isTossWon = true;
     alert("You won the toss");
   } else {
+    isTossWon = false;
     alert("You loss the toss");
   }
 }
@@ -66,8 +85,64 @@ for (let i = 0; i < tosses.length; i++) {
   toss.addEventListener("click", tossClickHandler);
 }
 
+function electComputerChoice() {
+  const choiceNumber = Math.round(Math.random());
+  return choices[choiceNumber];
+}
+
 function tossNextClickHanlder() {
   tossWrapper.remove();
+  if (isTossWon) {
+    electedWrapper.classList.replace("hide", "show");
+  } else {
+    const computerChoice = electComputerChoice();
+    if (computerChoice === "bat") {
+      userPlaying = "bowl";
+    } else {
+      userPlaying = "bat";
+    }
+    alert(
+      `Computer has elected to ${computerChoice} first. You will ${userPlaying} first.`,
+    );
+    playWrapper.classList.replace("hide", "show");
+  }
 }
 
 tossNextButton?.addEventListener("click", tossNextClickHanlder);
+
+// Elected (if toss is won)
+function electedClickHandler(event) {
+  const choice = event.target;
+  const choiceId = choice.id;
+  userPlaying = choiceId;
+}
+const electingChoices = electedWrapper.getElementsByClassName("side");
+for (let i = 0; i < electingChoices.length; i++) {
+  const choice = electingChoices[i];
+  choice.addEventListener("click", electedClickHandler);
+}
+
+function electedNextClickHanlder() {
+  electedWrapper.remove();
+  alert(`You have elected to ${userPlaying} first.`);
+  playWrapper.classList.replace("hide", "show");
+}
+electedNextButton?.addEventListener("click", electedNextClickHanlder);
+
+// Play
+function playClickHandler(event) {
+  const isSpaceClicked = event.code === "Space";
+  if (!isSpaceClicked) return;
+
+  const playItemType = Math.floor(Math.random() * 3);
+  const playItem = playItems[playItemType];
+  console.log(playItem, "===playItem");
+
+  const playItemDetail = playItemDetails[playItem];
+}
+
+playClickButton.addEventListener("click", function () {
+  document.addEventListener("keydown", playClickHandler);
+  alert("Press Spacebar to play the balls");
+  playClickButton.remove();
+});
--- js/cricket-game/callback.js
+++ js/cricket-game/callback.js
@@ -1,16 +1,37 @@
-console.log("1");
+// console.log("1");
+// let c;
 
-function ourCallback(c) {
-  const result = c * 2;
-  console.log(result)
-}
+// function ourCallback(e) {
+//   // const result = c * 2;
+//   console.log(result);
+// }
 
-setTimeout(function () {
-  const a = 1;
-  const b = 2;
-  const c = a + b;
-  console.log("2");
-  ourCallback();
-}, 3000);
+// setTimeout(function () {
+//   const a = 1;
+//   const b = 2;
+//   c = a + b;
+//   ourCallback(c);
+// }, 3000);
 
-console.log("3")
+// console.log("3");
+// console.log(c);
+
+// FALSY VALUES
+// empty string ""
+// 0 
+// null
+// undefined
+// NaN
+// false
+
+// TRUTHY VALUES
+// "hello"
+// 1
+// []
+// {}
+
+// if ([].a) {
+//   console.log("Hello");
+// } else {
+//   console.log("Hi");
+// }
