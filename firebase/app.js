63605e81729c9402d0ee646470a5ee6ece13f340
Abdullah Motiwala <motiwalaabdullah100@gmail.com>
2026-04-25 22:46:53 +0500

    firebase covered

--- firebase/app.js
+++ firebase/app.js
@@ -48,9 +48,9 @@ function handleSignin() {
     });
 }
 
-setInterval(() => {
-  console.log("Current user:", auth.currentUser);
-}, 2000);
+// setInterval(() => {
+//   console.log("Current user:", auth.currentUser);
+// }, 2000);
 
 const signoutButton = document.getElementById("signout-btn");
 signoutButton.addEventListener("click", handleSignout);
@@ -83,3 +83,112 @@ function handleAddDoc() {
       console.error("Error adding document:", error);
     });
 }
+
+const deleteDocButton = document.getElementById("deleteDoc-btn");
+deleteDocButton.addEventListener("click", handleDeleteDoc);
+
+function handleDeleteDoc() {
+  const userCollection = firestore.collection("users");
+  const doc = userCollection.doc("WcXEvmUaSjViFr2kF25a");
+
+  doc
+    .set()
+    .then(() => {
+      console.log("Document successfully deleted!");
+    })
+    .catch((error) => {
+      console.error("Error deleting document:", error);
+    });
+}
+
+const setDocButton = document.getElementById("setDoc-btn");
+setDocButton.addEventListener("click", handleSetDoc);
+
+function handleSetDoc() {
+  const userCollection = firestore.collection("users");
+  const doc = userCollection.doc("WcXEvmUaSjViFr2kF25a");
+
+  doc
+    .set({
+      user: "wasif",
+      age: 26,
+      married: false,
+    })
+    .then(() => {
+      console.log("Document successfully set!");
+    })
+    .catch((error) => {
+      console.error("Error setting document:", error);
+    });
+}
+
+const updateDocButton = document.getElementById("updateDoc-btn");
+updateDocButton.addEventListener("click", handleUpdateDoc);
+
+function handleUpdateDoc() {
+  const userCollection = firestore.collection("users");
+  const doc = userCollection.doc("tQZe4ANyFGbbjq9q4c7g");
+
+  doc
+    .update({
+      user: "abdullah",
+      age: 26,
+      married: false,
+    })
+    .then(() => {
+      console.log("Document successfully update!");
+    })
+    .catch((error) => {
+      console.error("Error updating document:", error);
+    });
+}
+
+const getDocButton = document.getElementById("getDoc-btn");
+getDocButton.addEventListener("click", handleGetDoc);
+
+function handleGetDoc() {
+  const userCollection = firestore.collection("users");
+  const doc = userCollection.doc("tQZe4ANyFGbbjq9q4c7f");
+
+  doc
+    .get({
+      user: "abdullah",
+      age: 26,
+      married: false,
+    })
+    .then((doc) => {
+      console.log("Document successfully get!", doc.data());
+    })
+    .catch((error) => {
+      console.error("Error updating document:", error);
+    });
+}
+
+const getAllDocButton = document.getElementById("getAllDoc-btn");
+getAllDocButton.addEventListener("click", handleGetAllDoc);
+
+function handleGetAllDoc() {
+  const userCollection = firestore.collection("users");
+
+  userCollection
+    .get()
+    .then((qs) => {
+      qs.forEach((doc)=>{
+        console.log(doc.data())
+      })
+    })
+    .catch((error) => {
+      console.error("Error getAll document:", error);
+    });
+}
+
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in
+    console.log("Logged in:", user.uid, user.email);
+  } else {
+    // User is signed out
+    console.log("Logged out");
+  }
+});
